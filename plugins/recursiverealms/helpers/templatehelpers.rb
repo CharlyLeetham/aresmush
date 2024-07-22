fe41c0544d1389ad38cb3180dee37f0bd001cf62
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-23 09:48:28 +1000

    Fixing helpders

--- plugins/recursiverealms/helpers/templatehelpers.rb
+++ plugins/recursiverealms/helpers/templatehelpers.rb
@@ -1,11 +1,9 @@
 module AresMUSH
     module RecursiveRealms
-      module TemplateHelpers
         def center_text(text, width)
           padding = (width - text.length) / 2
           ' ' * padding + text + ' ' * padding
         end
-      end
     end
-  end
+end
   
