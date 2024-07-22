a20b493a27a078cc50ac01b889cbd1cc6f72ec00
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-22 18:26:20 +1000

    going back further

--- plugins/recursiverealms/recursiverealms.rb
+++ plugins/recursiverealms/recursiverealms.rb
@@ -9,80 +9,69 @@ module AresMUSH
       end
   
       def self.get_cmd_handler(client, cmd, enactor)
-        case cmd.root
+
+        client.emit_ooc "Debug: Entered get_cmd_handler"
+        client.emit_ooc "Debug: cmd.root is '#{cmd.root}'"
+        client.emit_ooc "Debug: cmd.switch is '#{cmd.switch}'"
+        client.emit_ooc "Debug: cmd.args initial state is '#{cmd.args}'"
+        case cmd.root          
         when "rr"
-          case cmd.switch
+          case cmd.switch               
           when "start"
             return StartCmd
           when "types"
+            client.emit_ooc "Debug: cmd.switch is 'types'"
+            client.emit_ooc "Debug: cmd.args before check is '#{cmd.args}'"           
             if cmd.args
+
+            # Debugging output for cmd.args
+            client.emit_ooc "Debug: Command arguments are '#{cmd.args}'"
+            Global.logger.debug "Debug: Command arguments are '#{cmd.args}'"
+            return                
               if cmd.args =~ /^(\w+)\/(tiers|sa|moves|full)$/
                 type, detail = $1.downcase, $2.downcase
                 case detail
                 when "tiers"
-                  return ListTypeTiersCmd.new(client, cmd, enactor)
+                  return ListTypeTiersCmd
                 when "sa"
-                  return ListTypeSACmd.new(client, cmd, enactor)
+                  return ListTypeSACmd
                 when "moves"
-                  return ListTypeMovesCmd.new(client, cmd, enactor)
+                  return ListTypeMovesCmd
                 when "full"
-                  return ListTypeFullCmd.new(client, cmd, enactor)
-                end
-              elsif cmd.args =~ /^(\w+)$/
-                return ListTypeCmd.new(client, cmd, enactor)
-              else
-                client.emit_ooc "Error: Invalid command format."
-              end
-            else
-              # Split cmd.switch to handle cases like 'types/vector'
-              split_switch = cmd.switch.split('/')
-              if split_switch.length > 1
-                type = split_switch[1]
-                detail = split_switch.length > 2 ? split_switch[2] : nil
-  
-                case detail
-                when "tiers"
-                  return ListTypeTiersCmd.new(client, cmd, enactor)
-                when "sa"
-                  return ListTypeSACmd.new(client, cmd, enactor)
-                when "moves"
-                  return ListTypeMovesCmd.new(client, cmd, enactor)
-                when "full"
-                  return ListTypeFullCmd.new(client, cmd, enactor)
-                else
-                  # Handle case when only type is provided
-                  cmd.args = type
-                  return ListTypeCmd.new(client, cmd, enactor)
-                end
-              else
-                return ListAllTypesCmd.new(client, cmd, enactor)
-              end
-            end
-  
-            if cmd.args
-              # Debugging output for cmd.args
-              client.emit_ooc "Debug: Command arguments are '#{cmd.args}'"
-              
-              if cmd.args =~ /^(\w+)\/(tiers|sa|moves|full)$/
-                type, detail = $1.downcase, $2.downcase
-                case detail
-                when "tiers"
-                  return ListTypeTiersCmd.new(client, cmd, enactor)
-                when "sa"
-                  return ListTypeSACmd.new(client, cmd, enactor)
-                when "moves"
-                  return ListTypeMovesCmd.new(client, cmd, enactor)
-                when "full"
-                  return ListTypeFullCmd.new(client, cmd, enactor)
+                  return ListTypeFullCmd
                 end
               elsif cmd.args =~ /^(\w+)$/
                 self.type = $1.downcase
-                return ListTypeCmd.new(client, cmd, enactor)
+                return ListTypeCmd
               else
                 client.emit_ooc "Error: Invalid command format."
               end
             else
-              return ListAllTypesCmd.new(client, cmd, enactor)
+              return ListAllTypesCmd
             end
-   
+          when "select"
+            return SelectTypeCmd
+          when "tier"
+            return DisplayTierCmd
+          when "attributes"
+            return DisplayAttributesCmd
+          when "moves"
+            return SelectMovesCmd
+          end
+        end
+        nil
+      end
   
+      def self.get_event_handler(event_name)
+        nil
+      end
+  
+      def self.get_web_request_handler(request)
+        nil
+      end
+  
+      def self.get_master_help
+        return { "RecursiveRealms" => t('recursive_realms.help') }
+      end
+    end
+end
