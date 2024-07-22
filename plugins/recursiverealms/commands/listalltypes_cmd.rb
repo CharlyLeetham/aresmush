b04c65a3fcc6da48e7b383cff0db14f83893c7c4
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-22 20:58:42 +1000

    List All Types command

--- plugins/recursiverealms/commands/listalltypes_cmd.rb
+++ plugins/recursiverealms/commands/listalltypes_cmd.rb
@@ -2,6 +2,7 @@ module AresMUSH
   module RecursiveRealms
     class ListAllTypesCmd
       include CommandHandler
+
       def initialize(client, cmd, enactor)
         @client = client
         @cmd = cmd
@@ -10,9 +11,8 @@ module AresMUSH
 
       def handle
         characters_config = Global.read_config("RecursiveRealms", "characters")
-
-        template = CharacterTypeTemplate.new(characters_config)
-        client.emit template.render        
+        template = CharacterTypesTemplate.new(characters_config)
+        client.emit template.render
       end
     end
   end
