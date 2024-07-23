2a961558fb0667bebb76aa2eceb6ddfc99f96a39
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-23 19:42:19 +1000

    Turning Character to Char

--- plugins/recursiverealms/template/character_types_template.rb
+++ plugins/recursiverealms/template/character_types_template.rb
@@ -1,15 +1,15 @@
 module AresMUSH
     module RecursiveRealms
       class CharacterTypesTemplate < ErbTemplateRenderer
-        attr_accessor :character
+        attr_accessor :char
   
-        def initialize(character)
-          @character = character
+        def initialize(char)
+          @char = char
           super File.dirname(__FILE__) + "/character_types.erb"
         end
 
         def chartypetitle
-          return @character["Type"]
+          return @char["Type"]
         end
 
       end
