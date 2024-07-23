d86aa46094f63360c34a8cde2dca16e6858305d1
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-23 10:45:57 +1000

    helpers

--- plugins/recursiverealms/helpers/templatehelpers.rb
+++ plugins/recursiverealms/helpers/templatehelpers.rb
@@ -1,8 +1,13 @@
 module AresMUSH
     module RecursiveRealms
+
         def self.center_text(text, width)
           padding = (width - text.length) / 2
           ' ' * padding + text + ' ' * padding
+        end
+
+        def self.greet(name)
+            "Hello, #{name}!"
         end
     end
 end
