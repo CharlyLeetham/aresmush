1a5cbe42e0bbc8364a1db6aaa3b0cd51d822fdfe
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-23 20:45:35 +1000

    can we wrap text?

--- plugins/recursiverealms/helpers/templatehelpers.rb
+++ plugins/recursiverealms/helpers/templatehelpers.rb
@@ -5,6 +5,12 @@ module AresMUSH
         def self.greet(name)
             "Hello, #{name}!"
         end
+
+        def self.wrap_text(text, line_width = 80)
+            text.scan(/.{1,#{line_width}}(?:\s+|$)|\S+/).each do |line|
+            puts line.strip
+          end
+        end
     end
 end
   
