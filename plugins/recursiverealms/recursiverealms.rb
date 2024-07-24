22868d9a82a82e932bdf2441e559624a8708a727
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-24 20:15:35 +1000

    Testing

--- plugins/recursiverealms/recursiverealms.rb
+++ plugins/recursiverealms/recursiverealms.rb
@@ -22,8 +22,8 @@ module AresMUSH
                 split_switch = cmd.switch.split('/').reject(&:empty?)
                 if split_switch.length > 1
                     fr = split_switch[0]
-                    detail = split_switch.length > 2 ? split_switch[1] : nil
-                    attrib = split_switch.length > 3 ? split_switch[2] : nil
+                    detail = split_switch.length > 1 ? split_switch[1] : nil
+                    attrib = split_switch.length > 2 ? split_switch[2] : nil
                     client.emit_ooc "Debug: type initial state is '#{split_switch[0]}', '#{fr}'"
                     client.emit_ooc "Debug: detail initial state is '#{split_switch[1]}', '#{detail}'"
                     client.emit_ooc "Debug: attrib initial state is '#{split_switch[2]}', '#{attrib}'"
