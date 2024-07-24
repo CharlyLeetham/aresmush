294be7f32a0e12546b8bb99e2a4c4f898a5ed214
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-24 20:33:58 +1000

    moar testing

--- plugins/recursiverealms/commands/listype_cmd.rb
+++ plugins/recursiverealms/commands/listype_cmd.rb
@@ -6,19 +6,20 @@ module AresMUSH
       attr_accessor :detail
 
       def initialize(detail, client, cmd)
-        @detail = detail.downcase
+        @detail = detail
         @client = client
         @cmd = cmd    
       end
       
       def handle
-        chartype = Global.read_config("RecursiveRealms", "characters").find { |c| c['Type'].downcase == @detail }
-        if chartype
-              template = CharacterTypeTemplate.new(chartype)
-              client.emit template.render
-        else
-          @client.emit_failure "Character type not found."
-        end
+        @client.emit_failure "#{detail}"
+        #chartype = Global.read_config("RecursiveRealms", "characters").find { |c| c['Type'].downcase == @detail }
+        #if chartype
+        #      template = CharacterTypeTemplate.new(chartype)
+        #      client.emit template.render
+        #else
+        #  @client.emit_failure "Character type not found."
+        #end
       end     
     end
   end
