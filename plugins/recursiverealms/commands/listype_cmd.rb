db42e9811e71f662484b2082ddf32f98641d6d9c
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-30 21:30:53 +1000

    d

--- plugins/recursiverealms/commands/listype_cmd.rb
+++ plugins/recursiverealms/commands/listype_cmd.rb
@@ -3,7 +3,7 @@ module AresMUSH
     class ListTypeCmd 
       include CommandHandler
 
-      attr_accessor :topcmd, :type, :value, :charly   
+      attr_accessor :topcmd, :type, :value 
 
       def parse_args
           split_switch = cmd.switch.split('/').reject(&:empty?)
@@ -11,20 +11,18 @@ module AresMUSH
             self.topcmd = split_switch[0]
             self.type = split_switch.length > 1 ? split_switch[1].downcase : nil
             self.value = split_switch.length > 2 ? split_switch[2].downcase : nil
-            self.charly = "Bottom loop"
           end           
       end
 
       def handle
-        @client.emit_ooc "topcmd: #{topcmd}, type: #{type}, value: #{value}, charly: #{charly}"
+        @client.emit_ooc "topcmd: #{topcmd}, type: #{type}, value: #{value}"
         chartype = Global.read_config("RecursiveRealms", "characters").find { |c| c['Type'].downcase == self.type }
-        @client.emit_ooc chartype.inspect
-        #if chartype
-        #      template = CharacterTypeTemplate.new(chartype)
-        #      client.emit template.render
-        #else
-        #  @client.emit_failure "Character type not found."
-        #end
+        if chartype
+              template = CharacterTypeTemplate.new(chartype)
+              client.emit template.render
+        else
+          @client.emit_failure "Character type #{type.capitalize) not found. Please check your spelling."
+        end
       end     
     end
   end
