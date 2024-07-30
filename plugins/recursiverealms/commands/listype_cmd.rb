29cbc1ba30344949d2acd2fe9bbc48a298cfa137
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-30 20:59:35 +1000

    I hate arg parsing

--- plugins/recursiverealms/commands/listype_cmd.rb
+++ plugins/recursiverealms/commands/listype_cmd.rb
@@ -3,23 +3,25 @@ module AresMUSH
     class ListTypeCmd 
       include CommandHandler
 
-      attr_accessor :topcmd, :type, :value      
+      attr_accessor :topcmd, :mine, :value, :charly   
 
       def parse_args
         if (cmd.args =~ /\//)
           args = cmd.parse_args(ArgParser.arg1_equals_arg2_slash_optional_arg3)
           self.topcmd = titlecase_arg(args.arg1)
-          self.type = titlecase_arg(args.arg2)
+          self.mine = titlecase_arg(args.arg2)
           self.value = titlecase_arg(args.arg3)
+          self.charly = "Top loop"
         else
           args = cmd.parse_args(ArgParser.arg1_equals_optional_arg2)
           self.topcmd = enactor_name
-          self.type = titlecase_arg(args.arg1)
+          self.mine = titlecase_arg(args.arg1)
+          self.charly = "Bottom loop"
         end
       end
 
       def handle
-        @client.emit_ooc "topcmd: #{topcmd}, type: #{type}, value: #{value}"
+        @client.emit_ooc "topcmd: #{topcmd}, type: #{mine}, value: #{value}, charly: #{charly}"
         #chartype = Global.read_config("RecursiveRealms", "characters").find { |c| c['Type'].downcase == @detail }
         #if chartype
         #      template = CharacterTypeTemplate.new(chartype)
