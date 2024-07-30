a991cc9a334eb0087cba5d5c2ef468609995c1d1
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-07-30 21:29:15 +1000

    logic

--- plugins/recursiverealms/commands/listype_cmd.rb
+++ plugins/recursiverealms/commands/listype_cmd.rb
@@ -3,29 +3,28 @@ module AresMUSH
     class ListTypeCmd 
       include CommandHandler
 
-      attr_accessor :topcmd, :type, :value   
+      attr_accessor :topcmd, :type, :value, :charly   
 
       def parse_args
-        if (cmd.args =~ /\//)
           split_switch = cmd.switch.split('/').reject(&:empty?)
           if split_switch.length > 1
             self.topcmd = split_switch[0]
             self.type = split_switch.length > 1 ? split_switch[1].downcase : nil
             self.value = split_switch.length > 2 ? split_switch[2].downcase : nil
-        end           
-
-        end
+            self.charly = "Bottom loop"
+          end           
       end
 
       def handle
-        @client.emit_ooc "topcmd: #{topcmd}, type: #{type}, value: #{value}"
+        @client.emit_ooc "topcmd: #{topcmd}, type: #{type}, value: #{value}, charly: #{charly}"
         chartype = Global.read_config("RecursiveRealms", "characters").find { |c| c['Type'].downcase == self.type }
-        if chartype
-              template = CharacterTypeTemplate.new(chartype)
-              client.emit template.render
-        else
-          @client.emit_failure "Character type #{type} not found. Please check your spelling and try again."
-        end
+        @client.emit_ooc chartype.inspect
+        #if chartype
+        #      template = CharacterTypeTemplate.new(chartype)
+        #      client.emit template.render
+        #else
+        #  @client.emit_failure "Character type not found."
+        #end
       end     
     end
   end
