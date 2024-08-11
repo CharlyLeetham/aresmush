62980df1aaf3a6ea113f8b3cf4c7f4a4b2e19426
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-08-11 13:03:54 +1000

    rrr

--- plugins/recursiverealms/recursiverealms.rb
+++ plugins/recursiverealms/recursiverealms.rb
@@ -22,7 +22,8 @@ module AresMUSH
           when ->(args) { args.start_with?('types') }       
                 split_switch = RecursiveRealms.split_command(cmd)
                 client.emit_ooc "split_switch: #{split_switch.length}"
-                client.emit_ooc "split_switch: #{split_switch}"                
+                client.emit_ooc "split_switch: #{split_switch}"    
+                client.emit_ooc "fr: #{split_switch[0]}, detail: #{split_switch[1]}, attrib: #{split_switch[2]}"                            
                 if split_switch.length > 1
                     fr = split_switch[0]
                     detail = split_switch.length > 1 ? split_switch[1] : nil
