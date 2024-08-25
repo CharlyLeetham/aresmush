451f6fc9c05027fa5a6479bdeb4199b4656547e2
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-08-25 14:57:29 +1000

    hhh

--- plugins/recursiverealms/helpers/helpers.rb
+++ plugins/recursiverealms/helpers/helpers.rb
@@ -2,7 +2,7 @@ module AresMUSH
     module RecursiveRealms
 
         def self.split_command(cmd)
-            split_switch = cmd.switch.split('/').reject(&:empty?)
+            split_switch = cmd.switch.split('/', 3).reject(&:empty?)
             arg1 = split_switch[0]
             arg2 = split_switch.length > 1 ? split_switch[1].downcase : nil
             arg3 = split_switch.length > 2 ? split_switch[2].downcase : nil
