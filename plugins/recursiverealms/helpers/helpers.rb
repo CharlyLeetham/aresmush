8a23577bd3ecc3d5cd785873a052734d60f15d41
CharlyLeetham <helpdesk@askcharlyleetham.com>
2024-08-25 15:04:08 +1000

    maybe this will work

--- plugins/recursiverealms/helpers/helpers.rb
+++ plugins/recursiverealms/helpers/helpers.rb
@@ -4,7 +4,7 @@ module AresMUSH
         def self.split_command(cmd)
             split_switch = cmd.switch.split('/', 3).reject(&:empty?)
             arg1 = split_switch[0]
-            arg2 = split_switch.length > 1 ? split_switch[1].downcase : nil
+            arg2 = split_switch.length > 1 ? split_switch[1..-1].join('/').downcase : nil
             arg3 = split_switch.length > 2 ? split_switch[2].downcase : nil
            return [arg1, arg2, arg3]
         end
